9add924e01ded8498bfda04c8063fdd8249a770a
Laksara <40088875+Laksara1996@users.noreply.github.com>
2020-06-02 20:27:56 +0530

    Profile Basic

--- src/components/accounts/ProfileComponent.js
+++ src/components/accounts/ProfileComponent.js
@@ -1,20 +1,44 @@
 import React, { Component } from 'react';
-import { Nav, NavItem, Dropdown, DropdownItem, DropdownToggle, DropdownMenu, NavLink } from 'reactstrap';
+import { Nav, Container, NavItem, NavLink, Card, Col, CardImg, Row, CardBody, CardTitle } from 'reactstrap';
+import 'react-datepicker/dist/react-datepicker.css';
+
+import image1 from '../../assests/profile/pr1.jpg';
 
 class Profile extends Component {
 	render() {
 		return (
-			<div>
-				<React.Fragment>
+			<Container>
+				<div className="row">
 					<Nav pills>
-						<NavItem>
-							<NavLink className="nav-link" to="/home">
-								Link
-							</NavLink>
-						</NavItem>
+						<div className="row">
+							<div className="col">
+								<NavItem>
+									<NavLink active href="/profile" className="nav-link">
+										Profile
+									</NavLink>
+								</NavItem>
+							</div>
+							<div className="col">
+								<NavItem>
+									<NavLink active href="/editprofile">
+										Edit Profile
+									</NavLink>
+								</NavItem>
+							</div>
+						</div>
 					</Nav>
-				</React.Fragment>
-			</div>
+					<Row>
+						<div className="col-12 col-md-5 m-1">
+							<Card>
+								<CardImg top src={image1} alt="None" />
+							</Card>
+						</div>
+						<div className="col-12 col-md-5 m-1">
+							<h3>Gayal Laksara</h3>
+						</div>
+					</Row>
+				</div>
+			</Container>
 		);
 	}
 }
